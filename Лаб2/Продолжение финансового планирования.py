f35e9f5ca845442bdec89e2c620f0d98708451df
KatherinePalinchuk <148626046+KatherinePalinchuk@users.noreply.github.com>
2023-11-07 08:29:03 +0300

    Update Продолжение финансового планирования.py

--- Лаб2/Продолжение финансового планирования.py
+++ Лаб2/Продолжение финансового планирования.py
@@ -6,7 +6,6 @@ increase = 0.03  # Ежемесячный рост цен
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 money_capital = 0
 for _ in range(months):
-    difference = spend - salary
+    money_capital += (spend - salary)
     spend *= increase + 1
-    money_capital += difference
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital, 2))
